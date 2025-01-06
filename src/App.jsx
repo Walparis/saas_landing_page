039c3949a23a9336cf5c4582b67b737c031e26f0
Walparis <walparis1@mail.ru>
2025-01-06 16:44:41 +0500

    added testimonial, download and footer sections

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,21 @@ import Hero from "./sections/Hero.jsx";
 import Features from "./sections/Features.jsx";
 import Pricing from "./sections/Pricing.jsx";
 import Faq from "./sections/Faq.jsx";
+import Testimonials from "./sections/Testimonials.jsx";
+import Download from "./sections/Download.jsx";
+import Footer from "./sections/Footer.jsx";
 
 const App = () => {
   return (
     <main className="overflow-hidden">
-      <Header></Header>
-      <Hero></Hero>
-      <Features></Features>
-      <Pricing></Pricing>
-      <Faq></Faq>
+      <Header />
+      <Hero />
+      <Features />
+      <Pricing />
+      <Faq />
+      <Testimonials />
+      <Download />
+      <Footer />
     </main>
   );
 };
--- src/sections/Pricing.jsx
+++ src/sections/Pricing.jsx
@@ -60,7 +60,7 @@ const Pricing = () => {
           </div>
 
           {/* Pricing section */}
-          <div className="scroll-hide relative z-2 -mt-12 flex items-start max-xl:gap-5 max-xl:overflow-auto max-xl:pt-6">
+          <div className="scroll-hide relative z-2 -mt-12 flex items-start max-xl:gap-5 max-xl:overflow-auto max-xl:pt-6 lg:justify-center">
             {plans.map((plan, index) => (
               <div
                 key={plan.id}
